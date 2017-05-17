4adfa20b0cb66db7f5cee5954ccfe8872475f9e7
agstephens <ag.stephens@stfc.ac.uk>
2017-05-17 14:29:45 +0100

    Added pyessv readme, integrated with pyessv

--- cc_plugin_primavera/primavera.py
+++ cc_plugin_primavera/primavera.py
@@ -5,7 +5,10 @@ compliance_checker.primavera
 Compliance Test Suite for the PRIMAVERA project
 """
 
-from compliance_checker.base import BaseCheck, BaseNCCheck, Result
+from compliance_checker.base import BaseCheck, BaseNCCheck, Result, TestCtx
+
+# Import library to interact with Controlled Vocabularies
+import pyessv
 
 class PrimCheck(BaseNCCheck):
     register_checker = True
@@ -17,7 +20,13 @@ class PrimCheck(BaseNCCheck):
         return Result(level, (score, out_of), name, messages)
 
     def setup(self, ds):
-        pass
+        self._cache_controlled_vocabularies()
+
+    def _cache_controlled_vocabularies(self):
+        """
+        Loads controlled vocabularies once and caches them.
+        """
+        self._wcrp_cmip6_cv = pyessv.load('wcrp', 'cmip6')
 
     def check_global_attributes(self, ds):
         """
@@ -50,4 +59,29 @@ class PrimCheck(BaseNCCheck):
         
         return self.make_result(level, score, out_of, 'Required Global Attributes', messages)
 
+    def check_frequency_global_attribute(self, ds):
+        """
+        Verifies value of the 'frequency' global attribute is
+        from CMIP6 vocabulary.
+        """
+        term = 'frequency'
+
+        ctx = TestCtx(category=BaseCheck.MEDIUM, description='Checks "{}" global att vs CV'.format(term))
+        allowed_values = [trm.label for trm in self._wcrp_cmip6_cv[term].terms]
+        gattr = ds.getncattr(term)
+
+        test_name = 'Checks "{}" global att vs CV'.format(term)
+        out_of = 2
+
+        if not gattr:
+            return self.make_result(BaseCheck.MEDIUM, 0, out_of, test_name,
+                                    ['Global attribute "{}" not found.'.format(term)])
+
+        if gattr not in allowed_values:
+            return self.make_result(BaseCheck.MEDIUM, 1, out_of, test_name,
+                                    ['Global attribute "{}" not in allowed values.'.format(term)])
+
+        return self.make_result(BaseCheck.MEDIUM, 2, out_of, test_name, [])
+        
+        
 
